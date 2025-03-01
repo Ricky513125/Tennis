2e8b27e1d07039248b98ff505118ba85de88ba48
Ricky513125 <lilingyu513125@163.com>
2025-03-01 13:22:29 +0800

    to extract_frames

--- extract_frames.py
+++ extract_frames.py
@@ -7,15 +7,19 @@ warnings.filterwarnings('ignore')
 
 # save images
 def save_imgs(cap, start, end, file_name, dim=224):
-    if not os.path.isdir('./vid_frames_%d' % dim):
-        os.mkdir('./vid_frames_%d' % dim)
-    path = os.path.join('./vid_frames_%d' % dim, file_name)
+    # if not os.path.isdir('./vid_frames_%d' % dim):
+    #     os.mkdir('./vid_frames_%d' % dim)
+    # path = os.path.join('./vid_frames_%d' % dim, file_name)
+    if not os.path.isdir('/mnt/ssd2/lingyu/Tennis/vid_frames_%d' % dim):
+        os.mkdir('/mnt/ssd2/lingyu/Tennis/vid_frames_%d' % dim)
+    path = os.path.join('/mnt/ssd2/lingyu/Tennis/vid_frames_%d' % dim, file_name)
     if not os.path.isdir(path):
         os.mkdir(path)
     count = 0
     cap.set(1, start)
     for t in range(start, end):
-        frame_path = './vid_frames_%d/%s/%06d.jpg' % (dim, file_name, count)
+        # frame_path = './vid_frames_%d/%s/%06d.jpg' % (dim, file_name, count)
+        frame_path = '/mnt/ssd2/lingyu/Tennis/vid_frames_%d/%s/%06d.jpg' % (dim, file_name, count)
         # if not os.path.exists(frame_path):
         _, frame = cap.read()
         H, W, _ = frame.shape
@@ -28,10 +32,12 @@ dim = 224
 for i, name in enumerate(data_name):
     print(name)
     out = []
-    json_file = json.load(open('./data/f3tennis/%s.json' % name))
+    # json_file = json.load(open('./data/f3tennis/%s.json' % name))
+    json_file = json.load(open('/mnt/ssd2/lingyu/Tennis/%s.json' % name))
     for clip in tqdm(json_file):
         match_id = '_'.join(clip['video'].split('_')[:-2])
-        video_name = './videos/%s.mp4' % match_id
+        # video_name = './videos/%s.mp4' % match_id
+        video_name = '/mnt/ssd2/videos/f3set-tennis/%s.mp4' % match_id
         cap = cv2.VideoCapture(video_name)
         # save images
         start, end = int(clip['video'].split('_')[-2]), int(clip['video'].split('_')[-1])
