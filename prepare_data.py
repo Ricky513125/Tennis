e283102c5e1b359959846f869d748e774a7f3eeb
Ricky513125 <lilingyu513125@163.com>
2025-03-01 18:44:30 +0800

    change to b_hybrid_pt.sh

--- prepare_data.py
+++ prepare_data.py
@@ -5,7 +5,7 @@ df = pd.read_csv('./data/videos.csv')
 
 # 只取最前面的video_name一列
 new_df = df[['video_name']].copy()
-
+new_df['video_name'] = '/mnt/ssd2/videos/f3set-tennis/' + new_df['video_name']
 # 在第二列全填0
 new_df['new_column_2'] = 0
 
--- scripts/pretrain/vit_b_hybrid_pt.sh
+++ scripts/pretrain/vit_b_hybrid_pt.sh
@@ -4,29 +4,27 @@ set -x
 export MASTER_PORT=$((12000 + $RANDOM % 20000))
 export OMP_NUM_THREADS=1
 
-OUTPUT_DIR='YOUR_PATH/work_dir/vit_b_hybrid_pt_800e'
-DATA_PATH='YOUR_PATH/data/hybrid_train.csv'
+OUTPUT_DIR='/mnt/ssd2/lingyu/Tennis/work_dir/vit_b_hybrid_pt_800e'
+DATA_PATH='/mnt/ssd2/lingyu/Tennis/data/hybrid_train.csv'
 
 JOB_NAME=$1
 PARTITION=${PARTITION:-"video"}
-# 8 for 1 node, 16 for 2 node, etc.
-GPUS=${GPUS:-32}
-GPUS_PER_NODE=${GPUS_PER_NODE:-8}
+# 1 for single GPU
+GPUS=${GPUS:-1}
+GPUS_PER_NODE=${GPUS_PER_NODE:-1}
 CPUS_PER_TASK=${CPUS_PER_TASK:-12}
 SRUN_ARGS=${SRUN_ARGS:-""}
 PY_ARGS=${@:2}
 
-# batch_size can be adjusted according to the graphics card
-srun -p $PARTITION \
-        --job-name=${JOB_NAME} \
-        --gres=gpu:${GPUS_PER_NODE} \
-        --ntasks=${GPUS} \
-        --ntasks-per-node=${GPUS_PER_NODE} \
-        --cpus-per-task=${CPUS_PER_TASK} \
-        --kill-on-bad-exit=1 \
-        --async \
-        ${SRUN_ARGS} \
-        python -u run_mae_pretraining.py \
+# 8 for 1 node, 16 for 2 node, etc.
+#GPUS=${GPUS:-32}
+#GPUS_PER_NODE=${GPUS_PER_NODE:-8}
+#CPUS_PER_TASK=${CPUS_PER_TASK:-12}
+#SRUN_ARGS=${SRUN_ARGS:-""}
+#PY_ARGS=${@:2}
+
+
+python -u run_mae_pretraining.py \
         --data_path ${DATA_PATH} \
         --mask_type tube \
         --mask_ratio 0.9 \
@@ -34,7 +32,7 @@ srun -p $PARTITION \
         --decoder_mask_ratio 0.5 \
         --model pretrain_videomae_base_patch16_224 \
         --decoder_depth 4 \
-        --batch_size 32 \
+        --batch_size 4 \  # 减小 batch_size
         --num_sample 4 \
         --num_frames 16 \
         --sampling_rate 4 \
@@ -48,3 +46,37 @@ srun -p $PARTITION \
         --log_dir ${OUTPUT_DIR} \
         --output_dir ${OUTPUT_DIR} \
         ${PY_ARGS}
+
+
+# batch_size can be adjusted according to the graphics card
+#srun -p $PARTITION \
+#        --job-name=${JOB_NAME} \
+#        --gres=gpu:${GPUS_PER_NODE} \
+#        --ntasks=${GPUS} \
+#        --ntasks-per-node=${GPUS_PER_NODE} \
+#        --cpus-per-task=${CPUS_PER_TASK} \
+#        --kill-on-bad-exit=1 \
+#        --async \
+#        ${SRUN_ARGS} \
+#        python -u run_mae_pretraining.py \
+#        --data_path ${DATA_PATH} \
+#        --mask_type tube \
+#        --mask_ratio 0.9 \
+#        --decoder_mask_type run_cell \
+#        --decoder_mask_ratio 0.5 \
+#        --model pretrain_videomae_base_patch16_224 \
+#        --decoder_depth 4 \
+#        --batch_size 32 \
+#        --num_sample 4 \
+#        --num_frames 16 \
+#        --sampling_rate 4 \
+#        --num_workers 10 \
+#        --lr 1e-3 \
+#        --opt adamw \
+#        --opt_betas 0.9 0.95 \
+#        --warmup_epochs 20 \
+#        --save_ckpt_freq 20 \
+#        --epochs 200 \
+#        --log_dir ${OUTPUT_DIR} \
+#        --output_dir ${OUTPUT_DIR} \
+#        ${PY_ARGS}
