c56a1f2dfca6bfb074d07707e128f75318c8fe0a
Ricky513125 <lilingyu513125@163.com>
2025-03-23 21:13:43 +0800

    get_model_state_dict

--- get_state_dict.py
+++ get_state_dict.py
@@ -12,4 +12,15 @@ with open(output_path, "w") as f:
     for key in keys:
         f.write(key + "\n")
 
+if 'model' in state_dict:
+    model_state_dict = state_dict['model']
+    print(model_state_dict.keys())  # 这才是真正的模型权重 keys
+
+output_path = "model_state_dict_keys.txt"
+with open(output_path, "w") as f:
+    for key in model_state_dict.keys():
+        f.write(key + "\n")
+
+print(f"模型参数 keys 已保存到 {output_path}")
+
 print(f"Keys 已保存到 {output_path}")
