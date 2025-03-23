55c78e061f18cac209e243ad0662daef2f2ceb5e
Ricky513125 <lilingyu513125@163.com>
2025-03-23 22:32:08 +0800

    models/__init__

--- models/__init__.py
+++ models/__init__.py
@@ -6,18 +6,20 @@ from .modeling_finetune import (
     vit_small_patch16_224,
 )
 from .modeling_pretrain import (
-    pretrain_videomae_base_patch16_224,
-    # pretrain_videomae_giant_patch14_224,
-    pretrain_videomae_huge_patch16_224,
-    pretrain_videomae_large_patch16_224,
-    pretrain_videomae_small_patch16_224,
+    # pretrain_videomae_base_patch16_224,
+    # # pretrain_videomae_giant_patch14_224,
+    # pretrain_videomae_huge_patch16_224,
+    # pretrain_videomae_large_patch16_224,
+    # pretrain_videomae_small_patch16_224,
+    VideoMAE_ViT_B_1600,
 )
 
 __all__ = [
-    'pretrain_videomae_small_patch16_224',
-    'pretrain_videomae_base_patch16_224',
-    'pretrain_videomae_large_patch16_224',
-    'pretrain_videomae_huge_patch16_224',
+    'VideoMAE_ViT_B_1600',
+    # 'pretrain_videomae_small_patch16_224',
+    # 'pretrain_videomae_base_patch16_224',
+    # 'pretrain_videomae_large_patch16_224',
+    # 'pretrain_videomae_huge_patch16_224',
     # 'pretrain_videomae_giant_patch14_224',
     'vit_small_patch16_224',
     'vit_base_patch16_224',
