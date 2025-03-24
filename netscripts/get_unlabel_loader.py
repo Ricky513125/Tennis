23ecfcec8c77c9deb9831e6309f500b7bdad92e1
Ricky513125 <lilingyu513125@163.com>
2025-03-24 14:45:09 +0800

    tennis_unlabel_loader

--- netscripts/get_unlabel_loader.py
+++ netscripts/get_unlabel_loader.py
@@ -1,6 +1,7 @@
 from datamodule.unlabel_loader.epic_unlabel_loader import EPICUnlabelLoader
 from datamodule.unlabel_loader.meccano_unlabel_loader import MECCANOUnlabelLoader
 from datamodule.unlabel_loader.wear_unlabel_loader import WEARUnlabelLoader
+from datamodule.unlabel_loader.tennis_unlabel_loader import TENNISUnlabelLoader
 
 
 def get_unlabel_loader(cfg):
@@ -10,6 +11,8 @@ def get_unlabel_loader(cfg):
         unlabel_loader = MECCANOUnlabelLoader(cfg)
     elif cfg.target_dataset.lower() == "wear":
         unlabel_loader = WEARUnlabelLoader(cfg)
+    elif cfg.target_dataset.lower() == "tennis":
+        unlabel_loader = TENNISUnlabelLoader(cfg)
     else:
         raise Exception(f"{cfg.target_dataset} is not supported!")
     return unlabel_loader
