be26829299dc1ac4404c37e47af0572963c30b8b
Ricky513125 <lilingyu513125@163.com>
2025-03-25 15:17:52 +0800

    clip_start_frame

--- datamodule/dataset/tennis_dataset.py
+++ datamodule/dataset/tennis_dataset.py
@@ -192,7 +192,7 @@ class TennisDataset(torch.utils.data.Dataset):
         # add
         print(type(self.cfg.source_sampling_rate))  # 打印类型
         print('---num_frames', self.cfg.num_frames)
-
+        print('---clip_start_frame', self.cfg.clip_start_frame)
         source_frame_names = [
             max(1, source_clip_start_frame + self.cfg.source_sampling_rate * i)
             for i in range(self.cfg.num_frames)
