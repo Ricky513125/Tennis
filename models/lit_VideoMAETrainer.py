95145891cf393281c6600713ee509e92dbd28ee9
Ricky513125 <lilingyu513125@163.com>
2025-04-15 17:52:52 +0800

    03241

--- models/lit_VideoMAETrainer.py
+++ models/lit_VideoMAETrainer.py
@@ -194,8 +194,8 @@ C: 通道数。
             # print('lit_VideoMAETrainer.training_step', source_frames.shape)
             # 反归一化视频，将其恢复到原始数据范围[0, 1]
 
-            source_frames = input["source_frames"].permute(0, 3, 1, 2, 4)  # [B, C, T, H, W]
-            unlabel_frames = input["unlabel_frames"].permute(0, 3, 1, 2, 4)
+            source_frames = input["source_frames"].permute(0, 3, 2, 4, 1)  # [B, C, T, H, W]
+            unlabel_frames = input["unlabel_frames"].permute(0, 3, 2, 4, 1)
 
             # print(f"VideoMAETrainer 调整后 source_frames 形状: {source_frames.shape}")  # 应为 [4, 3, 16, 224, 384]
 
